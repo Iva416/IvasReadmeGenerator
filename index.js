aa92a7e6e6b1e1433bf3f255a05c5ee5ef3d4e79
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-10-05 19:12:31 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   { type: "What is the title of you project",
+    message: "",
+    name: "",
+}, 
+    { type: "What is the Description for your project?",
+    message: "",
+    name: "description",
+},
+    { type: "What are the installation instructions for your project?",
+    message: "",
+    name: "description",
+},
+    { type: "Contribution guidelines for your project?",
+    message: "",
+    name: "Guidelines",
+},
+    { type: "Usage of your project?",
+    message: "",
+    name: "Usage",
+},
+
+;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
