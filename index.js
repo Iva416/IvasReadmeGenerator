3fa286967a72eaf3c4b269e8ffd791bc91c68a82
Iva416 <112923663+Iva416@users.noreply.github.com>
2022-10-07 19:52:32 -0400

    Updated Code

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [
-   { type: "What is the title of you project",
-    message: "",
-    name: "",
+
+
+inquirer
+  .prompt([
+{
+    type: 'What is the title of you project',
+    message: '',
+    name: 'Title',
 }, 
-    { type: "What is the Description for your project?",
-    message: "",
-    name: "description",
+{ type: 'What is the Description for your project?',
+    message: '',
+    name: 'Description',
 },
-    { type: "What are the installation instructions for your project?",
-    message: "",
-    name: "description",
+    { type: 'What are the installation instructions for your project?',
+    message: '',
+    name: 'Installtion',
 },
-    { type: "Contribution guidelines for your project?",
-    message: "",
-    name: "Guidelines",
+    { type: 'Contribution guidelines for your project?',
+    message: '',
+    name: 'Contribution',
 },
-    { type: "Usage of your project?",
-    message: "",
-    name: "Usage",
+    { type: 'Usage of your project?',
+    message: '',
+    name: 'Usage',
 },
-
-;
+    { type: 'Tests for your project?',
+    message: '',
+    name: 'Test'
+}
+])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.html`;
 
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+}
 
 // Function call to initialize app
 init();
